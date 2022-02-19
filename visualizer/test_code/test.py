4105f7f9071bc41cf962afeefdea89cdc9c62473
RocaPiedra <pablo_rocasg97@hotmail.com>
2022-02-19 17:12:52 +0100

    hsv vs jet gradient test results

--- visualizer/test_code/test.py
+++
@@ -1,4 +0,0 @@
-import os
-print(os.path.join(os.path.dirname(__file__), '..', 'data', 'mydata.json'))
-print(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'data', 'mydata.json')))
-print(os.path.dirname(__file__))
