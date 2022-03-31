0a806b060b392aadbb14f0d912b1fc26f15b2b5e
RocaPiedra <pablo_rocasg97@hotmail.com>
2022-03-31 19:06:46 +0200

    updated menu with generic visualization pipeline

--- visualizer/menu.py
+++ visualizer/menu.py
@@ -3,9 +3,7 @@
 @original author: Utku Ozbulak - github.com/utkuozbulak
 """
 from time import sleep
-from PIL import Image
 from cv2 import *
-import numpy as np
 import torch
 import ntpath # to obtain the filename in a full path
 import os
@@ -60,30 +58,11 @@ if __name__ == '__main__':
     if option == 1:
         print('Webcam selected as input')
         cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)   # /dev/video0
-        while True:
+        while not parameters.activate_deleter:
             ret, frame = cap.read()
             if not ret:
                 break
-            original_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            cv2.imshow('Input',frame)
-            c = cv2.waitKey(1) # ASCII 'Esc' value
-            if c == 27:
-                print(f'Closing {cam.method_name}, shutting down application...')
-                cap.release()
-                cv2.destroyAllWindows()
-                exit()
-
-            prep_img = preprocess_image(original_image, parameters.sendToGPU)
-            cam_gen = cam.generate_cam(prep_img)
-            # Show mask
-            heatmap, heatmap_on_image = apply_colormap_on_image(original_image, cam_gen, 'hsv')
-            cv2_heatmap_on_image = cv2.cvtColor(np.array(heatmap_on_image), cv2.COLOR_RGB2BGR)
-            cv2.imshow(f'{cam.method_name}',cv2_heatmap_on_image)
-            if c == 27:
-                print(f'Closing {cam.method_name}, shutting down application...')
-                cap.release()
-                cv2.destroyAllWindows()
-                exit()
+            cam.visualization_pipeline(frame, None, True)
 
     elif option == 2:
         print('Image selected as input')
@@ -91,14 +70,13 @@ if __name__ == '__main__':
         image_paths = get_image_path(path,None)
         # do a function for target class
         for paths in image_paths:
-            original_image = Image.open(paths).convert('RGB')
-            prep_img = preprocess_image(original_image)
+            original_image = cv2.imread(paths)
             # take the name of the file without extension:
             file_name_to_export = os.path.splitext(ntpath.basename(paths))[0]
-            # Generate cam mask
-            cam_gen = cam.generate_cam(prep_img)
-            # Save mask
-            save_class_activation_images(original_image, cam_gen, file_name_to_export, pretrained_model.__class__.__name__)
+            # process the image and obtain the overlayed with cam method
+            cv2_heatmap_on_image = cam.visualization_pipeline(original_image, None, True)
+            path_to_file = os.path.join('../results', file_name_to_export+'_'+cam.method_name+'_'+pretrained_model.__class__.__name__+'.png')
+            save_image(cv2_heatmap_on_image, path_to_file)
             print(f'{cam.method_name} completed for image:',file_name_to_export)
 
     elif option == 3:
@@ -106,35 +84,19 @@ if __name__ == '__main__':
         frame_counter = 0
         video_path = '../input_images/video_input/Road-traffic.mp4'
         cap = cv2.VideoCapture(video_path)
-        while cap.isOpened():
+        while cap.isOpened() and not parameters.activate_deleter:
             ret, frame = cap.read()
             frame_counter += 1
             if not ret:
                 break
-            original_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            cv2.imshow('Input',frame)
-            
-            prep_img = preprocess_image(original_image)
-            file_name_to_export = f'webcam_{frame_counter}'
-            cam_gen = cam.generate_cam(prep_img)
-            # Show mask
-            heatmap, heatmap_on_image = apply_colormap_on_image(original_image, cam_gen, 'hsv')
-            cv2_heatmap_on_image = cv2.cvtColor(np.array(heatmap_on_image), cv2.COLOR_RGB2BGR)
-            cv2.imshow(f'{cam.method_name}',cv2_heatmap_on_image)
-
-            c = cv2.waitKey(1) # ASCII 'Esc' value
-            if c == 27:
-                print(f'Closing {cam.method_name}, shutting down application...')
-                cap.release()
-                cv2.destroyAllWindows()
-                exit()
+            cam.visualization_pipeline(frame, None, True)
     
     elif option == 4:
         print('Carla selected as input')
         subp_unreal, subp_traffic = launch_carla_simulator_locally()
         platform = sensor_platform()
         sensor = platform.set_sensor()
-        sensor.listen(lambda data: cam.visualization_pipeline(data, platform,True))
+        sensor.listen(lambda data: cam.visualization_pipeline(data, platform, True, False))
         while not parameters.activate_deleter:sleep(2)
         
         subp_unreal.terminate()
